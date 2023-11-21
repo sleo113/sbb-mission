9a1a734d2e94b9a6e5fa05565f417d15463aeb65
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 09:32:00 +0900

    Chap 2-2, 1

---
+++ src/main/java/com/ll/sbbmission4/MainController.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    public void index() {
+        System.out.println("index");
+    }
+}
