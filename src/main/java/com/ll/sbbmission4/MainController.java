4e455d2879d40bcb3496a142a179db2e7401f8fe
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 09:34:08 +0900

    CHap 2-2, 2

--- src/main/java/com/ll/sbbmission4/MainController.java
+++ src/main/java/com/ll/sbbmission4/MainController.java
@@ -2,12 +2,14 @@ package com.ll.sbbmission4;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/sbb")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody
+    public String index() {
+        return "index";
     }
 }
