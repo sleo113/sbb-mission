cc28e3f59890afd7e3671633bce5b7369f493c4a
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 13:43:43 +0900

    Chap 2-5, finById

--- src/test/java/com/ll/sbbmission4/SbbMission4ApplicationTests.java
+++ src/test/java/com/ll/sbbmission4/SbbMission4ApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,10 +17,10 @@ class SbbMission4ApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
 }
