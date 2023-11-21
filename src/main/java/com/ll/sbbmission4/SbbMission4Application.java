9955f31a0bbc1d9d7fa253c41745d335001222c3
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 09:17:58 +0900

    Chap 2-1

---
+++ src/main/java/com/ll/sbbmission4/SbbMission4Application.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMission4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMission4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbbmission4/SbbMission4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbbmission4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMission4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
