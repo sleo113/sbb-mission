61f40188145d60331928269abab13ebfae81c4d5
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 09:53:27 +0900

    Chap 2-4, Add Answer

--- src/main/java/com/ll/sbbmission4/Answer.java
+++ src/main/java/com/ll/sbbmission4/Answer.java
@@ -9,17 +9,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 200)
-    private String subject;
-
     @Column(columnDefinition = "TEXT")
     private String content;
 
-
     private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
 }
