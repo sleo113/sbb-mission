e145f532eb5d3d0d09c84d248d72ed2e511cd55c
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-21 10:13:03 +0900

    chap 2-4, Finish

--- src/main/java/com/ll/sbbmission4/Question.java
+++ src/main/java/com/ll/sbbmission4/Question.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,6 +20,6 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-
-    private LocalDateTime createDate;
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
