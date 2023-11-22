73f438f867b11cc02fbeba473d7fdff806b6008f
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-22 13:24:22 +0900

    Chap 2-7, Use templates

--- src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
+++ src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
@@ -2,14 +2,13 @@ package com.ll.sbbmission4.sbb_question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
 
     @GetMapping("/question/list")
-    @ResponseBody
+    //@ResponseBody
     public String list() {
-        return "question list";
+        return "question_list";
     }
 }
