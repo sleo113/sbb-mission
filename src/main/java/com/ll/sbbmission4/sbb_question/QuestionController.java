4102671acab9141cdc17de21c0f61659dba1aea0
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-22 14:42:06 +0900

    Chap 2-10, question/detail/2

--- src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
+++ src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
@@ -2,8 +2,9 @@ package com.ll.sbbmission4.sbb_question;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,5 +19,10 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
