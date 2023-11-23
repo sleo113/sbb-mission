83b3bb2b317a29750373972d7288d8bb83467a5d
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-23 11:27:04 +0900

    Chap 2-15, Finish

--- src/main/java/com/ll/sbbmission4/sbb_answer/AnswerController.java
+++ src/main/java/com/ll/sbbmission4/sbb_answer/AnswerController.java
@@ -2,14 +2,14 @@ package com.ll.sbbmission4.sbb_answer;
 
 import com.ll.sbbmission4.sbb_question.Question;
 import com.ll.sbbmission4.sbb_question.QuestionService;
-
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/answer")
 @RequiredArgsConstructor
@@ -20,9 +20,14 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+    public String createAnswer(Model model, @PathVariable("id") Integer id,
+                               @Valid AnswerForm answerForm, BindingResult bindingResult) {
         Question question = this.questionService.getQuestion(id);
-        this.answerService.create(question, content);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
+            return "question_detail";
+        }
+        this.answerService.create(question, answerForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
 }
