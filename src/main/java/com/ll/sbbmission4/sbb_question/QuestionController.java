7daf052a3a832599b09987cff6896d38169d739f
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-23 10:35:58 +0900

    Chap 2-15, modify templates & service

--- src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
+++ src/main/java/com/ll/sbbmission4/sbb_question/QuestionController.java
@@ -3,9 +3,7 @@ package com.ll.sbbmission4.sbb_question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,11 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
