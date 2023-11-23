d1c89ba2506802fae5b69c92cf15ecef8535cef0
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-23 17:07:14 +0900

    Chap 3-6, Add Sign Up form, Sign Up Controller

--- src/main/java/com/ll/sbbmission4/user/UserService.java
+++ src/main/java/com/ll/sbbmission4/user/UserService.java
@@ -1,21 +1,20 @@
 package com.ll.sbbmission4.user;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Site_User create(String username, String email, String password) {
         Site_User user = new Site_User();
         user.setUsername(username);
         user.setEmail(email);
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
