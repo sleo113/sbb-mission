6380002cb5416abe520ac69f4538b30f38ff2379
JeUk Park <qkrwpdnr4531@naver.com>
2023-11-23 17:23:02 +0900

    Chap 3-7, modify UserRepository

--- src/main/java/com/ll/sbbmission4/user/UserRepository.java
+++ src/main/java/com/ll/sbbmission4/user/UserRepository.java
@@ -2,5 +2,8 @@ package com.ll.sbbmission4.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Site_User, Long> {
+    Optional<Site_User> findByusername(String username);
 }
